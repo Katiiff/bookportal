7486bfc59dfcf2e4974c21c7105909457a4c1b11
aleshina-e <aleshina-e@datagile.ru>
2024-10-30 11:50:59 +0300

    создать основу приложения

--- src/main/java/com/example/bookportal/BookportalApplication.java
+++ src/main/java/com/example/bookportal/BookportalApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BookportalApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(BookportalApplication.class, args);
 	}
 
