49aa8d3f9fa5faaf1e049961e1363aa0946bd722
aleshina-e <aleshina-e@datagile.ru>
2024-11-13 09:56:29 +0300

    создать новую сущность

---
+++ src/main/java/com/example/bookportal/repositories/AuthorRepository.java
@@ -0,0 +1,14 @@
+package com.example.bookportal.repositories;
+
+import com.example.bookportal.models.Author;
+import com.example.bookportal.models.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+    Optional<Author> findByNameAndSurname(String authorName, String authorSurname);
+}
